8047c513b4358e5a5368b4a246d8b23f317f71a7
laratarpley13 <ltarp13@gmail.com>
2020-10-29 13:13:17 -0500

    added jQuery library and link index.js in index.html; added function framework in index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+// Event Listeners
+function newItem(){
+    //listen for new item submission
+    $("#js-shopping-list-form").submit(function(e){
+        e.preventDefault(); //prevent default submission
+        //add entered item as an li element in the ul parent element
+        $(".shopping-list").append(`<li><span class="shopping-item">${this}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`)
+    });
+}
+
+function checkItems(){
+    //listen for user to check or check of item
+    $("").click(function(e){
+        e.preventDefault(); //prevent default submission
+    });
+}
+
+function removeItems(){
+    //listen for user to delete item
+    $("").click(function(e){
+        e.preventDefault(); //prevent default submission
+    });
+}
+
+//main function
+function main(){
+    
+}
+
+//Evoke the main function
+$(main);
